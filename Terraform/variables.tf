586eeab44d9e8ee6f1a35f7f2f1577f9b7904157
souravsingh028 <91778398+souravsingh028@users.noreply.github.com>
2024-06-08 19:45:30 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -22,4 +22,5 @@ variable "cluster_name" {
 variable "ecs_task_execution_role_arn" {
   description = "ARN of the ECS task execution role"
   type        = string
+  default     = "your-ecs-task-execution-role-arn"
 }
